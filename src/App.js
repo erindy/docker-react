6f35c305cb4685df7c14e6cdde07f25ffa12c066
erindmehmeti <erindmehmeti@gmail.com>
2020-05-23 23:19:53 +0200

    testing fd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!!
+          Ehi amore bello ti amo tanto!!
         </p>
         <a
           className="App-link"
