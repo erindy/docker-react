a2459c3a517784cbece1a36e182e9a3563d63518
erindmehmeti <erindmehmeti@gmail.com>
2020-05-23 23:32:19 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ehi amore bello ti amo tanto!!
+          I was changed on the Feature branch!
         </p>
         <a
           className="App-link"
